71db6e9b1f32dfc10451af398fd64573f58dd344
echo <hujw77@gmail.com>
2023-10-13 10:44:26 +0800

    deploy

--- script/Deploy.s.sol
+++ script/Deploy.s.sol
@@ -18,8 +18,8 @@ contract Deploy is Common {
     using ScriptTools for string;
 
     address immutable ORMP = 0x0000000000BD9dcFDa5C60697039E2b3B28b079b;
-    address immutable ADDR = 0x006D35B3e76301D3AFa9Dae8498434605DE7A8d4;
-    bytes32 immutable SALT = 0x6b40720786789ec67c8acca7e51158d80488969b2ade753eef6108ef77a0c280;
+    address immutable ADDR = 0x00043b475D815bA0269786e2edC908209b778Fa8;
+    bytes32 immutable SALT = 0x1357173166d559904de46d4d1a01f3168992187a0a6149e8426ce9c044724e25;
 
     string config;
     string instanceId;
@@ -59,7 +59,7 @@ contract Deploy is Common {
         require(subapi == ADDR, "!addr");
 
         require(III(subapi).owner() == deployer);
-        console.log("SubAPI  deployed at %s", subapi);
+        console.log("SubAPI deployed: %s", subapi);
         return subapi;
     }
 
