0a292679c6582e865c0cf6042a49da480c7ac632
echo <hujw77@gmail.com>
2023-10-18 14:22:10 +0800

    confi

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -2,5 +2,5 @@
 
 set -eo pipefail
 
-forge script script/Deploy.s.sol:Deploy --chain-id 43     --broadcast --verify
-forge script script/Deploy.s.sol:Deploy --chain-id 421613 --broadcast --verify
+forge script script/Deploy.s.sol:Deploy --chain-id 44     --broadcast --verify
+forge script script/Deploy.s.sol:Deploy --chain-id 421614 --broadcast --verify
