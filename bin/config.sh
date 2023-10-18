f050146aa4750d242e3cb9c97daf592898947883
echo <hujw77@gmail.com>
2023-10-18 17:02:16 +0800

    uncomment

--- bin/config.sh
+++ bin/config.sh
@@ -2,5 +2,5 @@
 
 set -eo pipefail
 
-# forge script script/Config.s.sol:Config --chain-id 44     --broadcast
+forge script script/Config.s.sol:Config --chain-id 44     --broadcast
 forge script script/Config.s.sol:Config --chain-id 421614 --broadcast --skip-simulation
