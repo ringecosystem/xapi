4ace9a5193f27aa4ed225c67c175991558624ec4
echo <hujw77@gmail.com>
2023-10-19 17:49:35 +0800

    add beacons

--- bin/beacon/add-beacon-nada.sh
+++ bin/beacon/add-beacon-nada.sh
@@ -3,7 +3,7 @@
 set -eo pipefail
 
 airnode=0xd0431d757f6316821C69BD7477B9659357CFF69F
-sponsor=0x701d0d316C98Ca9df43Cb551b5E9F426ee689162
-sponsorWallet=0x4AddeA3541336C2cfAFeC454ffB2882549708A45
+sponsor=0x9F33a4809aA708d7a399fedBa514e0A0d15EfA85
+sponsorWallet=0xF9587bbf429D985809bba6a12CAaf71e0CC75187
 
-. $(dirname $0)/beacon/add-beacon.sh $airnode $sponsor $sponsorWallet
+. $(dirname $0)/add-beacon.sh $airnode $sponsor $sponsorWallet
--- bin/beacon/add-beacon-yalin.sh
+++ bin/beacon/add-beacon-yalin.sh
@@ -3,7 +3,7 @@
 set -eo pipefail
 
 airnode=0x9E1D2311c407AD0953f6BAa51209C014729D8343
-sponsor=0x9E1D2311c407AD0953f6BAa51209C014729D8343
-sponsorWallet=0x064115a572aCCacD5bb39236C46eE8A1D0631182
+sponsor=0x9F33a4809aA708d7a399fedBa514e0A0d15EfA85
+sponsorWallet=0xEadeF1aE573B3Eb6B609D93D7cAD909A798Ca428
 
-. $(dirname $0)/beacon/add-beacon.sh $airnode $sponsor $sponsorWallet
+. $(dirname $0)/add-beacon.sh $airnode $sponsor $sponsorWallet
