e38de686ceb2d8d034f9c3edb64371f313343ea3
echo <hujw77@gmail.com>
2023-10-19 13:27:46 +0800

    update script

--- bin/beacon/add-beacon.sh
+++ bin/beacon/add-beacon.sh
@@ -6,8 +6,9 @@ airnode=${1:?}
 sponsor=${2:?}
 sponsorWallet=${3:?}
 
-arbitest_chain=arbitest
-arbitest_dapi=0xa681492DBAd5a3999cFCE2d72196d5784dd08D0c
+subapi=0x007471Db6AD668b30a7CE648589a4C8C4f65a36f
+
+arbitest_chain=arbitrum-sepolia
 arbitest_endpointId=0x23e5743c946604a779a5181a1bf621076cd11687a1f21c8bc2fa483bd704b3ab
 
 arbitest_data=$(set -x; ethabi encode params \
@@ -15,16 +16,15 @@ arbitest_data=$(set -x; ethabi encode params \
   "(${airnode:2},${arbitest_endpointId:2},${sponsor:2},${sponsorWallet:2})")
 
 arbitest_sig=$(cast sig "addBeacon((address,bytes32,address,address))")
-(set -x; seth send $arbitest_dapi $arbitest_sig$arbitest_data --chain $arbitest_chain)
+(set -x; seth send $subapi $arbitest_sig$arbitest_data --chain $arbitest_chain)
 
 
-pangolin_chain=pangolin
-pangolin_dapi=0x770713580e5c618A4D29D7E8c0d7604276B63832
-pangolin_endpointId=0xe7fe8a321e9c000326638d5187a650e3f9d0652f30a01ad9ae4a60327e6c5277
+crab_chain=crab
+crab_endpointId=0xe7fe8a321e9c000326638d5187a650e3f9d0652f30a01ad9ae4a60327e6c5277
 
-pangolin_data=$(set -x; ethabi encode params \
+crab_data=$(set -x; ethabi encode params \
   -v "(address,bytes32,address,address)" \
-  "(${airnode:2},${pangolin_endpointId:2},${sponsor:2},${sponsorWallet:2})")
+  "(${airnode:2},${crab_endpointId:2},${sponsor:2},${sponsorWallet:2})")
 
-pangolin_sig=$(cast sig "addBeacon((address,bytes32,address,address))")
-(set -x; seth send $pangolin_dapi $pangolin_sig$pangolin_data --chain $pangolin_chain)
+crab_sig=$(cast sig "addBeacon((address,bytes32,address,address))")
+(set -x; seth send $subapi $crab_sig$crab_data --chain $crab_chain)
