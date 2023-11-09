d0235dfb5890984e1ff971be925e5b63c90422cb
echo <hujw77@gmail.com>
2023-11-09 11:52:02 +0800

    config on mainnet

--- bin/config.sh
+++ bin/config.sh
@@ -2,5 +2,5 @@
 
 set -eo pipefail
 
-forge script script/Config.s.sol:Config --chain-id 44     --broadcast
-forge script script/Config.s.sol:Config --chain-id 421614 --broadcast --skip-simulation
+forge script script/Config.s.sol:Config --chain-id 46    --broadcast --slow
+forge script script/Config.s.sol:Config --chain-id 42161 --broadcast --slow --legacy
