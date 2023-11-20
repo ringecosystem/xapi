7127cd903d037fe7dc08df11579d8c2a5a830588
echo <hujw77@gmail.com>
2023-11-20 11:53:53 +0800

    deploy on sepolia

--- bin/verify.sh
+++ bin/verify.sh
@@ -27,5 +27,4 @@ verify() {
     $path > script/output/$chain_id/$name.v.json)
 }
 
-verify $subapi 42161 $(cast abi-encode "constructor(address,address,address)" $deployer $rrp $ormp) src/SubAPI.sol:SubAPI
-verify $subapi 46    $(cast abi-encode "constructor(address,address,address)" $deployer $rrp $ormp) src/SubAPI.sol:SubAPI
+verify $subapi 11155111 $(cast abi-encode "constructor(address,address,address)" $deployer $rrp $ormp) src/SubAPI.sol:SubAPI
