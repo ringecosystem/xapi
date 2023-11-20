bcc5a289d45f5ba233df04c521889b84b1a7a486
echo <hujw77@gmail.com>
2023-11-20 12:02:52 +0800

    sepolia config

--- bin/config.sh
+++ bin/config.sh
@@ -2,5 +2,7 @@
 
 set -eo pipefail
 
-forge script script/Config.s.sol:Config --chain-id 46    --broadcast --slow
-forge script script/Config.s.sol:Config --chain-id 42161 --broadcast --slow --legacy
+# forge script script/Config.s.sol:Config --chain-id 46    --broadcast --slow
+# forge script script/Config.s.sol:Config --chain-id 42161 --broadcast --slow --legacy
+
+forge script script/Config.s.sol:Config --chain-id 11155111    --broadcast
