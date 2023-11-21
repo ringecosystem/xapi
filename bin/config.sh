aff800831f42408bbc0cd8fc6ad574b0a17bf0cc
echo <hujw77@gmail.com>
2023-11-21 14:17:31 +0800

    comment

--- bin/config.sh
+++ bin/config.sh
@@ -5,6 +5,6 @@ set -eo pipefail
 # forge script script/Config.s.sol:Config --chain-id 46    --broadcast --slow
 # forge script script/Config.s.sol:Config --chain-id 42161 --broadcast --slow --legacy
 
-forge script script/Config.s.sol:Config --chain-id 11155111 --broadcast
 forge script script/Config.s.sol:Config --chain-id 44       --broadcast
 forge script script/Config.s.sol:Config --chain-id 421614   --broadcast
+forge script script/Config.s.sol:Config --chain-id 11155111 --broadcast
