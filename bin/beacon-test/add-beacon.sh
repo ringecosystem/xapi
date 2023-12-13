a2dc27e18f90045d176dd6cc9f3be7499a1515eb
echo <hujw77@gmail.com>
2023-12-13 16:13:34 +0800

    Support Crab

--- bin/beacon-test/add-beacon.sh
+++ bin/beacon-test/add-beacon.sh
@@ -24,37 +24,42 @@ add_beacon() {
 }
 
 
+# chain=crab
+# # ArbitrumSepoliaMessageRoot
+# fromChain=arbitrum-sepolia
+# endpointId=0x63400292c39bafe31164968499615317be0f1b97b4122a29d436c8fb427fdf65
+# add_beacon $chain $endpointId $fromChain
+
+# # SepoliaMessageRoot
+# fromChain=sepolia
+# endpointId=0x009d6b53b1429850be54c01f42e00985b38c66c57d4742bcb443ee902ed7b7c8
+# add_beacon $chain $endpointId $fromChain
+
+
+# chain=arbitrum-sepolia
+# # CrabMessageRoot
+# fromChain=crab
+# endpointId=0xbd452be65dba2428924ea35129292b3247233952dae361d169f2d489d805ce23
+# add_beacon $chain $endpointId $fromChain
+
+# # SepoliaMessageRoot
+# fromChain=sepolia
+# endpointId=0x009d6b53b1429850be54c01f42e00985b38c66c57d4742bcb443ee902ed7b7c8
+# add_beacon $chain $endpointId $fromChain
+
+
+# chain=sepolia
+# # CrabMessageRoot
+# fromChain=crab
+# endpointId=0xbd452be65dba2428924ea35129292b3247233952dae361d169f2d489d805ce23
+# add_beacon $chain $endpointId $fromChain
+
+# # ArbitrumSepoliaMessageRoot
+# fromChain=arbitrum-sepolia
+# endpointId=0x63400292c39bafe31164968499615317be0f1b97b4122a29d436c8fb427fdf65
+# add_beacon $chain $endpointId $fromChain
+
 chain=crab
-# ArbitrumSepoliaMessageRoot
-fromChain=arbitrum-sepolia
-endpointId=0x63400292c39bafe31164968499615317be0f1b97b4122a29d436c8fb427fdf65
-add_beacon $chain $endpointId $fromChain
-
-# SepoliaMessageRoot
-fromChain=sepolia
-endpointId=0x009d6b53b1429850be54c01f42e00985b38c66c57d4742bcb443ee902ed7b7c8
-add_beacon $chain $endpointId $fromChain
-
-
-chain=arbitrum-sepolia
-# CrabMessageRoot
-fromChain=crab
-endpointId=0xbd452be65dba2428924ea35129292b3247233952dae361d169f2d489d805ce23
-add_beacon $chain $endpointId $fromChain
-
-# SepoliaMessageRoot
-fromChain=sepolia
-endpointId=0x009d6b53b1429850be54c01f42e00985b38c66c57d4742bcb443ee902ed7b7c8
-add_beacon $chain $endpointId $fromChain
-
-
-chain=sepolia
-# CrabMessageRoot
-fromChain=crab
-endpointId=0xbd452be65dba2428924ea35129292b3247233952dae361d169f2d489d805ce23
-add_beacon $chain $endpointId $fromChain
-
-# ArbitrumSepoliaMessageRoot
-fromChain=arbitrum-sepolia
-endpointId=0x63400292c39bafe31164968499615317be0f1b97b4122a29d436c8fb427fdf65
+fromChain=darwinia
+endpointId=0x45189e2288f2d2e384c9e3be7c3c6cef65a553341ca8580e1ed3516725112bb4
 add_beacon $chain $endpointId $fromChain
--- bin/beacon/add-beacon.sh
+++ bin/beacon/add-beacon.sh
@@ -32,14 +32,14 @@ add_beacon() {
 # endpointId=0xd3b815c7ac0ba9242a379bdc8a2f94d609e64239c6c85ac27e244828d6f48815
 # add_beacon $chain $endpointId $fromChain
 
-chain=arbitrum
-fromChain=darwinia
-endpointId=0x45189e2288f2d2e384c9e3be7c3c6cef65a553341ca8580e1ed3516725112bb4
-add_beacon $chain $endpointId $fromChain
+# chain=arbitrum
+# fromChain=darwinia
+# endpointId=0x45189e2288f2d2e384c9e3be7c3c6cef65a553341ca8580e1ed3516725112bb4
+# add_beacon $chain $endpointId $fromChain
 
-fromChain=ethereum
-endpointId=0xd3b815c7ac0ba9242a379bdc8a2f94d609e64239c6c85ac27e244828d6f48815
-add_beacon $chain $endpointId $fromChain
+# fromChain=ethereum
+# endpointId=0xd3b815c7ac0ba9242a379bdc8a2f94d609e64239c6c85ac27e244828d6f48815
+# add_beacon $chain $endpointId $fromChain
 
 # chain=ethereum
 # fromChain=darwinia
@@ -49,3 +49,8 @@ add_beacon $chain $endpointId $fromChain
 # fromChain=arbitrum
 # endpointId=0x18905d41e909c79069d74843dc474d0809df62b5bc555ea272b0cc49ff3fa924
 # add_beacon $chain $endpointId $fromChain
+
+chain=darwinia
+fromChain=crab
+endpointId=0xbd452be65dba2428924ea35129292b3247233952dae361d169f2d489d805ce23
+add_beacon $chain $endpointId $fromChain
