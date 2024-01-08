f7fd57e95dab9bb6ef3eaaf0b046cd6e1ce21c2e
echo <hujw77@gmail.com>
2024-01-08 15:29:32 +0800

    add chainId

--- src/SubAPISignaturePub.sol
+++ src/SubAPISignaturePub.sol
@@ -3,7 +3,7 @@
 pragma solidity 0.8.17;
 
 contract SubAPISignaturePub {
-    event SignatureSubmittion(address indexed signer, uint256 chainId, bytes signature, bytes data);
+    event SignatureSubmittion(uint256 indexed chainId, address indexed signer, bytes signature, bytes data);
 
     function submit(uint256 chainId, bytes calldata signature, bytes calldata data) external {
         emit SignatureSubmittion(chainId, msg.sender, signature, data);
