1857fe1f31d90cdb4b878e2a322e88a7161f0513
echo <hujw77@gmail.com>
2024-01-22 12:34:09 +0800

    Add a new parameter msgIndex of type uint256 to the event SignatureSubmittion

--- src/SubAPISignaturePub.sol
+++ src/SubAPISignaturePub.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.17;
 
 contract SubAPISignaturePub {
     event SignatureSubmittion(
-        uint256 indexed chainId, uint256 msgIndex, address indexed signer, bytes signature, bytes data
+        uint256 indexed chainId, uint256 indexed msgIndex, address indexed signer, bytes signature, bytes data
     );
 
     function submit(uint256 chainId, uint256 msgIndex, bytes calldata signature, bytes calldata data) external {
