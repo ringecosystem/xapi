b6c93d699338807cfd93334488bb7b9c1f584ea4
echo <hujw77@gmail.com>
2024-01-29 16:22:21 +0800

    deploy on testnet

---
+++ bin/multisig_deploy.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+
+# forge script script/Deploy.s.sol:Deploy --chain-id 46    --broadcast --verify --slow
+# forge script script/Deploy.s.sol:Deploy --chain-id 42161 --broadcast --verify --slow --legacy
+# forge script script/Deploy.s.sol:Deploy --chain-id 1     --broadcast --verify --slow --legacy
+
+forge script script/MultiSigDeploy.s.sol:MultiSigDeploy --chain-id 44       --broadcast --verify --skip-simulation
+forge script script/MultiSigDeploy.s.sol:MultiSigDeploy --chain-id 421614   --broadcast --verify --skip-simulation
+forge script script/MultiSigDeploy.s.sol:MultiSigDeploy --chain-id 11155111 --broadcast --verify --skip-simulation
