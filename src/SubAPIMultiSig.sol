7561456046aaf6ff96418aaeb1620fa3087b7e05
echo <hujw77@gmail.com>
2024-02-01 13:57:40 +0800

    using safe lib

--- src/SubAPIMultiSig.sol
+++ src/SubAPIMultiSig.sol
@@ -2,20 +2,21 @@
 
 pragma solidity 0.8.17;
 
-import "./MultiSig.sol";
+import {ECDSA} from "@openzeppelin/contracts@4.9.2/utils/cryptography/ECDSA.sol";
+import {OwnerManager} from "@safe-smart-account/base/OwnerManager.sol";
 import "./interfaces/IOracle.sol";
 
-contract SubAPIMultiSig is MultiSig {
+contract SubAPIMultiSig is OwnerManager {
     IOracle public immutable ORACLE;
 
     mapping(bytes32 => bool) doneOf;
 
-    error VerifySignaturesFailed();
     error HashAlreadyUsed(bytes32 hash);
     error OperationExpired(bytes32 hash);
 
-    constructor(address oracle, address[] memory _signers, uint64 _quorum) MultiSig(_signers, _quorum) {
+    constructor(address oracle, address[] memory _signers, uint64 _threshold) {
         ORACLE = IOracle(oracle);
+        _setupOwners(_signers, _threshold);
     }
 
     function _checkSigs(uint256 expiration, bytes32 hash, bytes calldata signatures) internal view {
@@ -25,10 +26,7 @@ contract SubAPIMultiSig is MultiSig {
         if (doneOf[hash]) {
             revert HashAlreadyUsed(hash);
         }
-        (bool sigsValid,) = verifySignatures(hash, signatures);
-        if (!sigsValid) {
-            revert VerifySignaturesFailed();
-        }
+        verifySignatures(hash, signatures);
     }
 
     function importMessageRoot(
@@ -56,5 +54,23 @@ contract SubAPIMultiSig is MultiSig {
         bytes32 hash = keccak256(data);
         _checkSigs(expiration, hash, signatures);
         ORACLE.setApproved(operator, approve);
+    }
+
+    function addOwnerWithThreshold(address owner, uint256 _threshold, bytes calldata signatures) external {}
+
+    function verifySignatures(bytes32 _hash, bytes calldata _signatures) public view {
+        require(_signatures.length == threshold * 65, "Invalid signature length");
+        bytes32 messageDigest = ECDSA.toEthSignedMessageHash(_hash);
+
+        address lastOwner = address(0);
+        for (uint256 i = 0; i < threshold; i++) {
+            bytes calldata signature = _signatures[i * 65:(i + 1) * 65];
+            address currentOwner = ECDSA.recover(messageDigest, signature);
+            require(
+                currentOwner > lastOwner && owners[currentOwner] != address(0) && currentOwner != SENTINEL_OWNERS,
+                "GS026"
+            );
+            lastOwner = currentOwner;
+        }
     }
 }
