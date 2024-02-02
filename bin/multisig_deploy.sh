53590dcb8ff5e19434f1422207d5158d1c01895e
echo <hujw77@gmail.com>
2024-02-02 16:29:05 +0800

    Redeploy on testnet

--- bin/multisig_deploy.sh
+++ bin/multisig_deploy.sh
@@ -7,6 +7,6 @@ set -eo pipefail
 # forge script script/Deploy.s.sol:Deploy --chain-id 1     --broadcast --verify --slow --legacy
 
 forge script script/MultiSigDeploy.s.sol:MultiSigDeploy --chain-id 43       --broadcast --verify --skip-simulation
-# forge script script/MultiSigDeploy.s.sol:MultiSigDeploy --chain-id 44       --broadcast --verify --skip-simulation
-# forge script script/MultiSigDeploy.s.sol:MultiSigDeploy --chain-id 421614   --broadcast --verify --skip-simulation
-# forge script script/MultiSigDeploy.s.sol:MultiSigDeploy --chain-id 11155111 --broadcast --verify --skip-simulation
+forge script script/MultiSigDeploy.s.sol:MultiSigDeploy --chain-id 44       --broadcast --verify --skip-simulation
+forge script script/MultiSigDeploy.s.sol:MultiSigDeploy --chain-id 421614   --broadcast --verify --skip-simulation
+forge script script/MultiSigDeploy.s.sol:MultiSigDeploy --chain-id 11155111 --broadcast --verify --skip-simulation
