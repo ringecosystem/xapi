66736c10b6d8273bd1bdd6f21013a536b583f8ce
echo <hujw77@gmail.com>
2024-02-28 16:37:04 +0800

    support polygon (#51)

--- bin/multisig_deploy.sh
+++ bin/multisig_deploy.sh
@@ -10,7 +10,8 @@ set -eo pipefail
 # forge script script/MultiSigDeploy.s.sol:MultiSigDeploy --chain-id 421614   --broadcast --verify --skip-simulation
 # forge script script/MultiSigDeploy.s.sol:MultiSigDeploy --chain-id 11155111 --broadcast --verify --skip-simulation
 
-forge script script/MultiSigDeploy.s.sol:MultiSigDeploy --chain-id 1     --broadcast --verify --legacy  --with-gas-price 31000000000
-forge script script/MultiSigDeploy.s.sol:MultiSigDeploy --chain-id 44   --broadcast --verify
-forge script script/MultiSigDeploy.s.sol:MultiSigDeploy --chain-id 46    --broadcast --verify
-forge script script/MultiSigDeploy.s.sol:MultiSigDeploy --chain-id 42161 --legacy --broadcast --verify --skip-simulation
+# forge script script/MultiSigDeploy.s.sol:MultiSigDeploy --chain-id 1     --broadcast --verify --legacy  --with-gas-price 31000000000
+# forge script script/MultiSigDeploy.s.sol:MultiSigDeploy --chain-id 44   --broadcast --verify
+# forge script script/MultiSigDeploy.s.sol:MultiSigDeploy --chain-id 46    --broadcast --verify
+forge script script/MultiSigDeploy.s.sol:MultiSigDeploy --chain-id 137    --broadcast --verify --with-gas-price 150000000000 
+# forge script script/MultiSigDeploy.s.sol:MultiSigDeploy --chain-id 42161 --legacy --broadcast --verify --skip-simulation
