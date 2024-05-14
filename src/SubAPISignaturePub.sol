4e982d621263748b2c81ad04a2d26e00153a906e
echo <hujw77@gmail.com>
2024-05-14 10:06:13 +0800

    V2 (#56)
    * clean deps
    
    * deploy sig pub
    
    * fix #54

--- src/SubAPISignaturePub.sol
+++ src/SubAPISignaturePub.sol
@@ -4,10 +4,17 @@ pragma solidity 0.8.17;
 
 contract SubAPISignaturePub {
     event SignatureSubmittion(
-        uint256 indexed chainId, uint256 indexed msgIndex, address indexed signer, bytes signature, bytes data
+        uint256 indexed chainId,
+        address indexed channel,
+        address indexed signer,
+        uint256 msgIndex,
+        bytes signature,
+        bytes data
     );
 
-    function submit(uint256 chainId, uint256 msgIndex, bytes calldata signature, bytes calldata data) external {
-        emit SignatureSubmittion(chainId, msgIndex, msg.sender, signature, data);
+    function submit(uint256 chainId, address channel, uint256 msgIndex, bytes calldata signature, bytes calldata data)
+        external
+    {
+        emit SignatureSubmittion(chainId, channel, msg.sender, msgIndex, signature, data);
     }
 }
