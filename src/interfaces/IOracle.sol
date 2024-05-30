fd3c943b91ef9ae5800436e578dae8ccb4a57eaa
echo <hujw77@gmail.com>
2024-05-30 14:25:35 +0800

    Clean (#57)

--- src/interfaces/IOracle.sol
+++
@@ -1,9 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity 0.8.17;
-
-interface IOracle {
-    function importMessageRoot(uint256 chainId, uint256 blockNumber, bytes32 messageRoot) external;
-    function changeOwner(address owner_) external;
-    function setApproved(address operator, bool approve) external;
-}
